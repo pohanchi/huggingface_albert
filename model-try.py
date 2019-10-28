3ec6d4bcbde2f84b64e2327b1758a3f3295dee09
pohanchi <aapp1420@gmail.com>
2019-10-28 22:16:52 +0800

    using pytorch to create model

---
+++ model-try.py
@@ -0,0 +1,19 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import argparse
+import torch
+
+from transformers import ALBertConfig, ALBertForPreTraining
+
+import logging
+logging.basicConfig(level=logging.INFO)
+PATH = "./pytorch_albert"
+
+config = ALBertConfig.from_json_file("../../albert_tf_official/google-research-albert/albert/albert_config.json")
+model = ALBertForPreTraining(config)
+model.load_state_dict(torch.load(PATH))
+for name, param in model.named_parameters():
+    if param.requires_grad:
+        print(name, param.size())
